1d16a2579565e81cf30f34162c75ec743a45475e
juliennpanev <83950947+juliennpanev@users.noreply.github.com>
2023-02-08 20:25:29 +0200

    updated pnl function

--- tsunami.py
+++ tsunami.py
@@ -42,8 +42,8 @@ class Tsunami:
     def getPositionNotionalAndUnrealizedPnl(self, address, option):
         pnl = requests.post(self.node + '/utils/script/evaluate/' + self.amm, json={
             "expr": "getPositionNotionalAndUnrealizedPnl(\"" + address + "\", " + str(option) + ")"}).json()
-        print(pnl)
-        return pnl['result']['value']['_2']
+        data = pnl['result']
+        return { "positionNotional" : data['value']['_1'], "unrealizedPnl" : data['value']['_2']}
 
     def calcRemainMarginWithFundingPaymentAndRolloverFee(self):
         position = self.getPosition(self.myAddress)
@@ -51,14 +51,23 @@ class Tsunami:
         positionMargin = position['_2']['value']
         positionLstUpdCPF = position['_4']['value']
         positionTimestamp = position['_5']['value']
-        unrealizedPnl = self.getPositionNotionalAndUnrealizedPnl(self.myAddress, 1)['value']
+        unrealizedPnl = self.getPositionNotionalAndUnrealizedPnl(self.myAddress, 1)['unrealizedPnl']
 
         res = requests.post(self.node + '/utils/script/evaluate/' + self.amm, json = { "expr": "calcRemainMarginWithFundingPaymentAndRolloverFee(" + str(positionSize) + ", "  + str(positionMargin) + ", " + str(positionLstUpdCPF) + ", " + str(positionTimestamp) + ", " + str(unrealizedPnl) + ")" }).json() 
         return res
+    
+    def getLastMinuteId(self):
+        req = requests.get('https://nodes.wavesnodes.com/addresses/data/3P9mFc9Zn9bsk9McUXECybF4imt7691VM1F/k_lastMinuteId').json()
+        return req['value']
+    
+    def getLastMinutePrice(self):
+        lastMinuteId = self.getLastMinuteId()
+        req = requests.get('https://nodes.wavesnodes.com/addresses/data/3P9mFc9Zn9bsk9McUXECybF4imt7691VM1F/k_twapDataLastPrice_' + str(lastMinuteId)).json()
+        print(req)
 
     def getMarginRatioByOption(self, address, option):
         marginRatio = requests.post(node + '/utils/script/evaluate/' + self.amm, json={
-            "expr": "getMarginRatioByOption(\"" + address + ", " + str(option) + ")"}).json()
+            "expr": "getMarginRatioByOption(\"" + address + "\", " + str(option) + ")"}).json()
         return marginRatio['result']['value']
 
     def getPositionSize(self, address):
@@ -87,9 +96,13 @@ myAddress = '3PP6kMgzK2d9zP4i5Zt7RtbpjaMZZzdhR63'
 
 tsunami = Tsunami(dapp, amm, myAddress=myAddress, node=node)
 
-a = tsunami.calcRemainMarginWithFundingPaymentAndRolloverFee()
+last = tsunami.getLastMinuteId()
 
-print(a)
+lastPrice = tsunami.getLastMinutePrice()
+marketPrice = tsunami.getMarketPrice()
+
+print(marketPrice)
+
 
 #              let positionSize = $t07705077174._1
 #              let positionMargin = $t07705077174._2
